4324981b2a07a12c5edfae9f5547e5997a928596
Ludusamo <ludusamo@gmail.com>
2014-04-24 08:26:21 -0400

    Clean up.

--- asteroid_sensor/asteroid_sensor.ino
+++ asteroid_sensor/asteroid_sensor.ino
@@ -38,21 +38,8 @@
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
-
-// Constants
-#define RED 0x1
-#define YELLOW 0x3
-#define GREEN 0x2
-#define TEAL 0x6
-#define BLUE 0x4
-#define VIOLET 0x5
-#define WHITE 0x7
 
 #define OPTICAL_PIN 0
-
-
-boolean X, Y, Z, HEADING, ALL;
 
 void displaySensorDetails(void) {
   sensor_t sensor;
@@ -92,7 +79,7 @@ void setup(void)
     while(1);
   }
   
-  delay(20 * 1000);
+  delay(10 * 1000);
   Serial.println("New Data: ");
 }
 
